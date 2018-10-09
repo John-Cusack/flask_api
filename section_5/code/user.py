a3b943f639f6ed851109fe23d6b5ff356075fcf3
john <johncusackccm@gmail.com>
2018-10-08 22:53:46 -0400

    test sqlite

--- section_5/code/user.py
+++ section_5/code/user.py
@@ -1,5 +1,54 @@
+import sqlite3
+from flask_restful import Resource
+
+
 class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
+    def __init__(self, _id, username, password):
+        self.id = _id
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(self, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(self, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    class UserRegister(Resource):
+        def post(self):
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor()
+
+            query = "INSERT INTO users VALUES (NULL, ?, ?)"
+            cursor.execute(query, data['username'], data['password'])
+
+            connection.commit()
+            connection.close
+
+            return {"message": "User created sucessfully"}, 201
