d6ee13b64ebb9fd6a6fa857d42b46bc775b14af0
john <johncusackccm@gmail.com>
2018-10-14 20:05:07 -0400

    finished up with sql

--- section_6/code/models/user.py
+++ section_6/code/models/user.py
@@ -1,15 +1,14 @@
-import sqlite3
 from db import db
 
 
 class UserModel(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    def __init__(self, _id, username, password):
-        self.id = _id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
@@ -23,4 +22,4 @@ class UserModel(db.Model):
 
     @classmethod
     def find_by_id(cls, _id):
-        return cls.query.filter_by(id=_id)
+        return cls.query.filter_by(id=_id).first()
--- section_6/code/resources/item.py
+++ section_6/code/resources/item.py
@@ -12,6 +12,12 @@ class Item(Resource):
                         help="This field cannot be left blank!"
                         )
 
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help="every item needs a store_id"
+                        )
+
     @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
@@ -25,7 +31,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
@@ -48,7 +54,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
         item.save_to_db()
