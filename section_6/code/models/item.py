363c9d68ba50851a8df0e1f33ca8a295f767f815
john <johncusackccm@gmail.com>
2018-10-14 20:29:34 -0400

    finished up with sql

--- section_6/code/models/item.py
+++ section_6/code/models/item.py
@@ -6,7 +6,8 @@ class ItemModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    price = db.Column(db.Float(precison=2))
+    price = db.Column(db.Float(precision=2))
+
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
     store = db.relationship('StoreModel')
 
--- section_6/code/resources/store.py
+++ section_6/code/resources/store.py
@@ -11,12 +11,13 @@ class Store(Resource):
 
     def post(self, name):
         if StoreModel.find_by_name(name):
-            return {'message': "A store with '{}' already exits".format(name)}, 400
+            return {'message': "A store with name '{}' already exists.".format(name)}, 400
+
         store = StoreModel(name)
         try:
             store.save_to_db()
         except:
-            return {'message': 'an error occurred while creating the store'}, 500
+            return {"message": "An error occurred creating the store."}, 500
 
         return store.json(), 201
 
@@ -24,9 +25,10 @@ class Store(Resource):
         store = StoreModel.find_by_name(name)
         if store:
             store.delete_from_db()
-        return {'message': 'store deleted'}
+
+        return {'message': 'Store deleted'}
 
 
 class StoreList(Resource):
     def get(self):
-        return {'stores': [store.json() for store in StoreModel.query.all()]}
+        return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
